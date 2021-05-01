f18d071056e02a3110c433abf508bbe017fcc373
Emma Ladouceur <emma.ladouceur@idiv.de>
2021-05-01 11:18:12 +0200

    spring clean

--- 3_Figures.R
+++ 3_Figures.R
@@ -7,12 +7,12 @@
 # #############     Produce Figures         ###############
 # _________________________________________________________
 
-# prackages
+# packages
 library(tidyverse)
 library(ggplot2)
 library(viridis)
 
-
+# data
 solution_dat <- read.csv("~/GRP GAZP Dropbox/Emma Ladouceur/_Projects/Marxan/Data/solution_dat.csv",header=T,fill=TRUE,sep=",",na.strings=c(""," ","NA","NA ","na"))
 random_dat <- read.csv("~/GRP GAZP Dropbox/Emma Ladouceur/_Projects/Marxan/Data/random_dat.csv",header=T,fill=TRUE,sep=",",na.strings=c(""," ","NA","NA ","na"))
 
@@ -50,8 +50,21 @@ ggplot(dat1,aes(x=objective, y=totalcf, color=objective),alpha=0.6,dotsize=0.5) 
   # scale_fill_manual(values=c("#E7298A","#66A61E","#7570B3","#D95F02","#E6AB02","#16A08CFF" ,"#15983DFF","#1F78B4","#666666")) +
   scale_color_manual(values=c("#D95F02","#E7298A","#7570B3","#16A08CFF", "#1F78B4","#E6AB02","#66A61E","#15983DFF","#666666")) +
      scale_fill_manual(values=c("#D95F02","#E7298A","#7570B3","#16A08CFF", "#1F78B4","#E6AB02","#66A61E","#15983DFF","#666666")) +
+  annotate("text", x= 3, y=77, label= "Prioritized Objectives", size= 5) +
+  annotate("segment", x = 0.5, xend = 2, y = 77, yend = 77, colour = "black")+
+  annotate("segment", x = 0.5, xend = 0.5, y = 77, yend = 75, colour = "black")+
+  annotate("segment", x = 4, xend = 5.25, y = 77, yend = 77, colour = "black")+
+  annotate("segment", x = 5.25, xend = 5.25, y = 77, yend = 75, colour = "black")+
+  annotate("text", x= 7.5, y=77, label= "Comparison Objectives", size= 5) +
+  annotate("segment", x = 5.75, xend = 6.5, y = 77, yend = 77, colour = "black")+
+  annotate("segment", x = 5.75, xend = 5.75, y = 77, yend = 75, colour = "black")+
+  annotate("segment", x = 8.5, xend = 9.25, y = 77, yend = 77, colour = "black")+
+  annotate("segment", x = 9.25, xend = 9.25, y = 77, yend = 75, colour = "black")+
   theme_classic() + theme(legend.position="none",
                           text = element_text(size=(20))) + scale_x_discrete(labels = function(x) str_wrap(x, width = 10))
+
+
+# LANDSCAPE 9X14
 
 # Expanded Fig 2 : Fig S1
 
@@ -71,10 +84,8 @@ dat2 <- dat1 %>%  filter(!objective == "Bird (n=5)"  ) %>% filter( !objective ==
   !objective == "Dominants (n=37)"  ) %>% filter( !objective == "Plant Rich Genus (n=115)"   ) %>% droplevels() %>%
   bind_rows(dat.sep)
 
-
 head(dat2)
 dat2$solution_amount <- factor(dat2$solution_amount, levels=c("5",".5","10","15","20","25","30","34","35",".35","37",".37","39","40","45","47","50","55","60","65","70","75","80","85","90","95","100","105","110","115",".115","116","119","120","121","125"))
-#dat2$objective <- factor(dat2$objective, levels=c("Bird (n=5)","Plant Rich Family (n=34)","Lepidoptera Relationship (n=35)", "Comprehensive (n=37)", "Dominants (n=37)",  "Pairwise Lepidoptera + Plant Rich Family (n=47)" , "Plant Rich Genus (n=115)","Pairwise Lepidoptera + Plant Rich Genus (n=119)", "Random"))
 
 dat2$objective <- factor(dat2$objective, levels=c( "Comprehensive (n=37)", "Bird (n=5)","Lepidoptera Relationship (n=35)", "Pairwise Lepidoptera + Plant Rich Family (n=47)" ,"Pairwise Lepidoptera + Plant Rich Genus (n=119)",
                                                    "Dominants (n=37)",  "Plant Rich Family (n=34)", "Plant Rich Genus (n=115)", "Random"))
@@ -84,16 +95,17 @@ ggplot(dat2,aes(x=solution_amount, y=totalcf, color=objective),alpha=0.6,dotsize
   geom_jitter(position = position_jitter(0.2),alpha=0.6) +
   stat_summary(fun.data="mean_sdl",  fun.args = list(mult=1), 
                fun.y=mean, geom="pointrange", size=0.6,  fill="black",color="black",alpha=0.6) + 
-  labs(x = '',y = 'Attribute Sum',fill='Objective',color='Objective') + 
+  labs(x = 'Number of plant species',y = 'Attribute Sum',fill='Objective',color='Objective') + 
   # scale_color_manual(values=c("#E7298A","#66A61E","#7570B3","#D95F02","#E6AB02","#16A08CFF" ,"#15983DFF","#1F78B4","#666666")) +
   # scale_fill_manual(values=c("#E7298A","#66A61E","#7570B3","#D95F02","#E6AB02","#16A08CFF" ,"#15983DFF","#1F78B4","#666666")) +
   scale_color_manual(values=c("#D95F02","#E7298A","#7570B3","#16A08CFF", "#1F78B4","#E6AB02","#66A61E","#15983DFF","#666666")) +
   scale_fill_manual(values=c("#D95F02","#E7298A","#7570B3","#16A08CFF", "#1F78B4","#E6AB02","#66A61E","#15983DFF","#666666")) +
   theme_classic() + theme(legend.position="bottom",
-                          text = element_text(size=(20))) +   guides(col = guide_legend(ncol = 3))
+                          text = element_text(size=(18))) +   guides(col = guide_legend(ncol = 3))
 
+# LANDSCAPE 9X14
 
-# Figure 3 : Irreplace-ability
+# Figure 3 : Irreplaceability
 head(master_dat)
 master_dat$sf <- as.numeric(master_dat$sf)
 importance_dat <- master_dat %>%  mutate( Importance = ifelse(master_dat$sf == 0, 'Redundant',
@@ -126,9 +138,8 @@ ggplot(importance_dat,aes(x=Importance, y=totalcf,color=Importance),alpha=0.6,do
                                                             axis.text.x=element_blank(),
                                                             axis.ticks.x=element_blank(),
                                                             text = element_text(size=(14)))
+
 #LANDSCAPE 8.50 X 12
-
-
 
 # FIGURE 4
 # MIXED LAYERS CLOUD + RANDOM MEAN + QUANTILES 
